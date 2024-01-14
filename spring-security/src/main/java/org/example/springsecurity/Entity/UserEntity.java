84b5b39fb38035543075528bc157d2f569ddbe63
RudrayyaSwamu <rudrayyaswamy248@gmail.com>
2024-01-14 11:06:36 +0530

    first spring security project

---
+++ spring-security/src/main/java/org/example/springsecurity/Entity/UserEntity.java
@@ -0,0 +1,22 @@
+package org.example.springsecurity.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+    private String roles;
+}
